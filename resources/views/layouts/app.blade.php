cb227fa5534ba840ee5d4fccf56f0a485a3b42f4
Janio Silva <janiosilva@outlook.com>
2017-07-23 17:30:25 -0300

    Resolve route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,26 +16,26 @@
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">G-Group</a>
+      <a class="navbar-brand" href="{{ url('/') }}">G-Group</a>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/grupos">Grupos</a>
+            <a class="nav-link" href="{{ url('/grupos') }}">Grupos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/noticias">Notícias</a>
+            <a class="nav-link" href="{{ url('/noticias') }}">Notícias</a>
           </li>
           
         </ul>
         <ul class="navbar-nav mr-auto navbar-toggler-right">
           <li class="nav-item active">
-            <a class="nav-link" href="/login">Login</a>
+            <a class="nav-link" href="{{ url('/login') }}">Login</a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="/register">Registro</a>
+            <a class="nav-link" href="{{ url('/register') }}">Registro</a>
           </li>
           
         </ul>
