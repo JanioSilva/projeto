aa0781ff61d619acc86d97bf563fed47b18055c2
Janio Silva <janiosilva@outlook.com>
2017-08-02 22:49:33 -0300

    Comecando a TimeLine

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,19 +15,23 @@
           <p>Universidade Federal de Alagoas</p>
           <hr class="item-divider">
           <div>
-            <nav class="nav flex-column">
+            <nav class="navbar-nav flex-column">
               <a class="nav-link active" data-toggle="collapse" href="#collapseGrupos" aria-expanded="false" aria-controls="collapseGrupos">Meus Grupos</a>
                <div class="collapse" id="collapseGrupos">
                   <div class="">
                     <ul class="list-group">
-                      @foreach(Auth::user()->grupos as $grupo)
-                        <li class="list-group-item">
-                          <a class="nav-link" href="/espelho/{{$grupo->id}}">
-                              <img class="rounded-circle mr-2" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="32" height="32">
-                              {{$grupo->nome}}
-                          </a>
-                        </li>
-                      @endforeach
+                        @foreach(Auth::user()->grupos as $grupo)  
+                              <a href="/espelho/{{$grupo->id}}" class="list-group-item list-group-item-action flex-column align-items-start">
+                                <div class="d-flex w-100 justify-content-between">
+                                  <h5 class="mb-1">{{$grupo->nome}}</h5>
+                                  <small class="text-muted">{{$grupo->created_at}}</small>
+                                </div>
+                                <p class="mb-1">{{$grupo->descricao}}</p>
+                                <small class="text-muted">Donec id elit non mi porta.</small>
+                              </a>
+                        @endforeach
+                      
+                      
                     </ul>
                   </div>
                </div>
@@ -46,10 +50,42 @@
           
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-8">
+        <div class="p-2">
           <div class="card card-block">
               <h3><i class="fa fa-tablet" aria-hidden="true"></i>  Aqui ficará a timeline</h3>
               <hr class="item-divider">
           </div>
+          
+            <div class="list-group">
+                <a href="#" class="mb-2 list-group-item list-group-item-action flex-column align-items-start active">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">List group item heading</h5>
+                    <small>3 days ago</small>
+                  </div>
+                  <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                  <small>Donec id elit non mi porta.</small>
+                </a>
+
+                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">List group item heading</h5>
+                    <small class="text-muted">3 days ago</small>
+                  </div>
+                  <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                  <small class="text-muted">Donec id elit non mi porta.</small>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">List group item heading</h5>
+                    <small class="text-muted">3 days ago</small>
+                  </div>
+                  <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                  <small class="text-muted">Donec id elit non mi porta.</small>
+                </a>
+              </div>
+
+            </div>{{-- fim do p-2   --}}
+
         </div>
         
       </div><!-- /.row -->
